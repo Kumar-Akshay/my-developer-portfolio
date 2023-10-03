8eb01bcc2a504308c1efac313d2dac36b3cb6a8c
Akshay Kumar <Akshaymalhi012@gmail.com>
2023-10-03 20:59:45 +0100

    navbar and home section in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import About from './components/About'
+import Skills from './components/Skills'
+import Work from './components/Work'
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Navbar/>
-    <Home/>
-    </>
+    <div name='home' className='w-full h-screen'>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Work/>
+      <Contact/>
+    </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,49 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import {BsDownload} from 'react-icons/bs';
-import TextChanger from './TextChanger';
+import TextChanger from './utilites/TextChanger';
+import ImageChanger from './utilites/ImageChanger';
 import Profile from '../assets/logo.jpg';
+import backgroundImage from '../assets/MyImages/Image1.jpg';
+import TypingAnimation from './utilites/TypingAnimation';
+
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-cover bg-center bg-[#0a192f]'>
       {/* Container */}
-      <div className="flex">
-        {/* Column 1 */}
+
+      <div className=' flex flex-row'>
+      {/* Column 1 */}
         <div className='max-w-[600px] my-[100px] mx-[150px]  flex flex-col justify-between h-full'>
             <h1 className='text-6xl text-pink-600'>Hi, </h1>
             <h1 className='text-2xl sm:text-7xl font-bold text-[#ccd6f6]'>
-            <TextChanger  textChangeStop = {true} texts = {["Akshay Kumar"]} speed={100} />
+              <TextChanger  textChangeStop = {true} texts = {["I'm Akshay Kumar"]} speed={100} />
             </h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-            <TextChanger textChangeStop = {false} title = {"I'm a"} texts={[ "Full Stack Developer",".NET Developer","Penetration Tester",]} speed={70}/>
-            </h2>
+            <TypingAnimation/>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>
             I’m a full-stack developer specializing in building (and occasionally
             designing) exceptional digital experiences. Currently, I’m focused on
             building responsive full-stack web applications.
             </p>
-            <div className='flex'>
+            <div className='flex flex-row-reverse'>
             <button className='text-white group border-2 px-6 py-3 my-2 mx-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                View Work.
+                View Work
                 <span className='group-hover:rotate-90 duration-300'>
                 <HiArrowNarrowRight className='ml-3 ' />
                 </span>
             </button>
             <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-                Download Resume.
+                Resume
                 <span>
                 <BsDownload className='ml-3 ' />
                 </span>
             </button>
             </div>
         </div>
-        {/* Column 2 */}
-        <div className="w-1/2 my-[100px] mx-[50px]">
-            <img src={Profile} alt="Your Image" className="w-50 h-50 rounded-full" />
-        </div>
-      </div>
-      
 
+      {/* Column 2 */}
+        <ImageChanger/>
+      </div>
     </div>
   );
 };
