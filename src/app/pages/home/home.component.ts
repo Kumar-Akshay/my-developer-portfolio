499f099ffee7c884d9b0636aaac8585de5f93d31
Kumar-Akshay <Akshaymalhi012@gmail.com>
2025-02-15 15:47:14 +0000

    home page animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,5 +11,11 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  ngOnInit() {
+    // Add Inter font
+    const link = document.createElement('link');
+    link.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+  }
 }
